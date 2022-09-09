b1ccd362c54b9a1f8f67cea20c9b50564cd42986
Lisa <lisa.guzeeva@mail.ru>
2022-09-09 09:30:14 +0300

    Второй commit.

---
+++ ComplexApp/ComplexApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using ComplexLib;
+
+namespace ComplexApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_button_Click(object sender, RoutedEventArgs e)
+        {
+            Complex z1 = new Complex(Convert.ToDouble(a.Text), Convert.ToDouble(b.Text));
+            Complex z2 = new Complex(Convert.ToDouble(c.Text), Convert.ToDouble(d.Text));
+            Complex res = z1 + z2;
+
+            result.Content = res.ToString();
+        }
+
+        private void Subtract_button_Click(object sender, RoutedEventArgs e)
+        {
+            Complex z1 = new Complex(Convert.ToDouble(a.Text), Convert.ToDouble(b.Text));
+            Complex z2 = new Complex(Convert.ToDouble(c.Text), Convert.ToDouble(d.Text));
+            Complex res = z1 - z2;
+
+            result.Content = res.ToString();
+        }
+
+        private void Multiply_button_Click(object sender, RoutedEventArgs e)
+        {
+            Complex z1 = new Complex(Convert.ToDouble(a.Text), Convert.ToDouble(b.Text));
+            Complex z2 = new Complex(Convert.ToDouble(c.Text), Convert.ToDouble(d.Text));
+            Complex res = z1 * z2;
+
+            result.Content = res.ToString();
+        }
+
+        private void Divide_button_Click(object sender, RoutedEventArgs e)
+        {
+            Complex z1 = new Complex(Convert.ToDouble(a.Text), Convert.ToDouble(b.Text));
+            Complex z2 = new Complex(Convert.ToDouble(c.Text), Convert.ToDouble(d.Text));
+            Complex res = z1 / z2;
+
+            result.Content = res.ToString();
+        }
+    }
+}
---
+++ ComplexLib/ComplexLib.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ComplexLib
+{
+    public class Complex
+    {
+        public readonly double a;
+        public readonly double b;
+
+        public Complex(double a = 0, double b = 0)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public static Complex operator +(Complex z1, Complex z2)
+        {
+            return new Complex(z1.a + z2.a, z1.b + z2.b);
+        }
+
+        public static Complex operator -(Complex z1, Complex z2)
+        {
+            return new Complex(z1.a - z2.a, z1.b - z2.b);
+        }
+
+        public static Complex operator *(Complex z1, Complex z2)
+        {
+            double real = z1.a * z2.a - z1.b * z2.b;
+            double imaginary = z1.b * z2.a + z1.a * z2.b;
+
+            return new Complex(real, imaginary);
+        }
+
+        public static Complex operator /(Complex z1, Complex z2)
+        {
+            double real = (z1.a * z2.a + z1.b * z2.b) / (Math.Pow(z2.a, 2) + Math.Pow(z2.b, 2));
+            double imaginary = (z1.b * z2.a - z1.a * z2.b) / (Math.Pow(z2.a, 2) + Math.Pow(z2.b, 2));
+
+            return new Complex(real, imaginary);
+        }
+
+        public override string ToString()
+        {
+            if (b < 0) return $"{a} - {-b}i";
+            else return $"{a} + {b}i";
+        }
+    };
+}
